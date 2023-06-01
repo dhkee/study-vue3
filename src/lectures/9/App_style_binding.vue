d38bca25f1a41e7a6cee3b1a913ff77504a3ae64
dhkee <dhkee@mcnc.co.kr>
2023-06-01 14:34:03 +0900

    9_class_style_binding

---
+++ src/lectures/9/App_style_binding.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<div :style="styleObject">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
+			cupiditate et officia laboriosam optio officiis aliquid amet fugiat, esse
+			deleniti? Deleniti fuga, nesciunt perferendis voluptatibus pariatur alias
+			molestias earum possimus!
+		</div>
+		<button v-on:click="fontSize--">-</button>
+		<button v-on:click="fontSize++">+</button>
+	</div>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue';
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// });
+		const fontSize = ref(13);
+
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+		return { styleObject, fontSize };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
